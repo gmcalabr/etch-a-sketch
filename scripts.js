03834e982907bb4e6696df6994b1b9aace600eca
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-21 17:42:04 -0500

    Completed mouseover color change. Pixels stay dark
    Code updated to include a mouseover event that is applied to each pixel
    so that each pixel turns colors. I have also added a mousedown event
    that is mostly useless for the moment. I will be committing this change
    and moving on to try and rework this event for better performance. The
    mouseover event tends to skip a lot of cells since the calculation
    is currently so taxing.

--- scripts.js
+++ scripts.js
@@ -1,37 +1,49 @@
-//Establish variables
-// Ask user for width of etch-a-sketch in pixels
-xPixels = prompt("What would you like your etch-a-sketch resolution to be (max 1000)");
-// Set total window width
-windowWidth = 1000;
-// Calculate the width/height of a 'pixel' in px
-pixelDims = Math.floor(windowWidth/xPixels);
-// For future programming reference, calculate total 'loss' by rounding pixel size down
-pixelError = (((windowWidth / xPixels) - Math.floor(windowWidth / xPixels)) * xPixels);
-// Calculate the total 'pixel' count in the grid
-totalPixels = xPixels ** 2;
-// Calculate the total 'overage' of field compared to pixelDims x xPixels
-fieldOverage = windowWidth - (pixelDims * xPixels);
-windowWidth = windowWidth - fieldOverage;
+document.addEventListener("DOMContentLoaded", declareVars());
 
+pixelCreatorLoop();
 
-// Resize wrap section to match even multiple of pixel size
-resizeWrap(windowWidth);
-
-// Loop the pixel creator
-let n = 0;
-while (n < totalPixels) {
-    addCell();
-    n++;
+function declareVars() {
+    //Establish variables
+    // Ask user for width of etch-a-sketch in pixels
+    xPixels = prompt("What would you like your etch-a-sketch resolution to be (max 1000)");
+    // Set total window width
+    windowWidth = 1000;
+    // Calculate the width/height of a 'pixel' in px
+    pixelDims = Math.floor(windowWidth/xPixels);
+    // For future programming reference, calculate total 'loss' by rounding pixel size down
+    pixelError = (((windowWidth / xPixels) - Math.floor(windowWidth / xPixels)) * xPixels);
+    // Calculate the total 'pixel' count in the grid
+    totalPixels = xPixels ** 2;
+    // Calculate the total 'overage' of field compared to pixelDims x xPixels
+    fieldOverage = windowWidth - (pixelDims * xPixels);
+    windowWidth = windowWidth - fieldOverage;
+    // Resize wrap section to match even multiple of pixel size
+    resizeWrap(windowWidth);
 };
 
+function onMouseOver(event) {
+    event.target.style.backgroundColor = "#282828";
+};
 
+function onMouseDown(event) {
+    event.target.style.backgroundColor = "#CC2222";
+};
 
 function resizeWrap(windowWidth) {
     // Function to resize the field to exact dimensions of the pixels
     // Without this, higher resolution displays risk having uneven pixel wrap
     const wrap = document.querySelector("#wrap");
     wrap.style.maxwidth = `${windowWidth}px`
-}
+};
+
+function pixelCreatorLoop() {
+// Loop the pixel creator
+    let n = 0;
+    while (n < totalPixels) {
+        addCell();
+        n++;
+    };
+};
 
 function addCell() {
     // Adds correct number of <div id="cell"> to the container
@@ -43,15 +55,9 @@ function addCell() {
     pixel.style.width = `${pixelDims}px`
     pixel.style.height = `${pixelDims}px`
 
-    addEventListener("mouseover", onMouseOver);
+    pixel.addEventListener("mouseover", onMouseOver);
+    pixel.addEventListener("mousedown", onMouseDown);
 
     container.appendChild(pixel);
     return;
-};
-
-
-const onMouseOver = (event) => {
-    // Define mouseover event
-    let target = event.target.innerText;
-    target.style.color = "red";
 };
